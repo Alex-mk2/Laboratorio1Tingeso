40be54a406125135ecf6e03d1922e7e522edcd95
Alex <alex.mellado.g@usach.cl>
2023-10-13 02:54:05 -0300

    [Alex Mellado]-Creacion de controladores y correción de pruebas service

--- Laboratorio1Tingeso-main/Laboratorio1 Tingeso/src/main/java/edu/mtisw/monolithicwebapp/controllers/PruebaController.java
+++ Laboratorio1Tingeso-main/Laboratorio1 Tingeso/src/main/java/edu/mtisw/monolithicwebapp/controllers/PruebaController.java
@@ -1,7 +1,42 @@
 package edu.mtisw.monolithicwebapp.controllers;
-
+import edu.mtisw.monolithicwebapp.entities.PruebaEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import edu.mtisw.monolithicwebapp.services.PruebaService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+
 
 @Controller
-public class PruebaController {
+@RestController
+public class PruebaController{
+
+    @Autowired
+    private PruebaService pruebaService;
+
+    @GetMapping("/uploadData")
+    public String uploadData(){
+        return "updateTest";
+    }
+    @PostMapping("/uploadData")
+    public String uploadData(@RequestParam("file") MultipartFile file) {
+        try {
+            pruebaService.GuardarNombreArchivo(file);
+            pruebaService.LeerArchivoCsv("Pruebas.csv");
+            return "Archivo cargado con éxito";
+        } catch (Exception e) {
+            return "Error al procesar el archivo: " + e.getMessage();
+        }
+    }
+
+    @GetMapping("/VisualizarPruebas")
+    public String mostrarPruebas(Model model){
+        ArrayList<PruebaEntity> Pruebas = pruebaService.ObtenerDatosPruebas();
+        model.addAttribute("pruebas", Pruebas);
+        return "mostrar-pruebas";
+    }
 }
+
--- Laboratorio1Tingeso-main/Laboratorio1 Tingeso/src/main/java/edu/mtisw/monolithicwebapp/services/PruebaService.java
+++ Laboratorio1Tingeso-main/Laboratorio1 Tingeso/src/main/java/edu/mtisw/monolithicwebapp/services/PruebaService.java
@@ -1,4 +1,5 @@
 package edu.mtisw.monolithicwebapp.services;
+
 import edu.mtisw.monolithicwebapp.entities.PruebaEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import lombok.Generated;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -21,13 +23,15 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+
 import edu.mtisw.monolithicwebapp.entities.estudianteEntity;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
+
 import java.util.Objects;
 
 @Service
-public class PruebaService {
+public class PruebaService{
     @Autowired
     pruebaRepository pruebaRepository;
     @Autowired
@@ -39,178 +43,161 @@ public class PruebaService {
         pruebaRepository.save(prueba);
     }
 
-    public ArrayList<PruebaEntity> obtenerEstudianteById(Long idEstudiante){
+    public ArrayList<PruebaEntity> obtenerEstudianteById(Long idEstudiante) {
         return pruebaRepository.findEstudentByid(idEstudiante);
     }
 
-    private final Logger logg = LoggerFactory.getLogger(PruebaService.class);
+    private final Logger logger = LoggerFactory.getLogger(PruebaService.class);
 
     @Generated
     public ArrayList<PruebaEntity> ObtenerDatosPruebas() {
         return (ArrayList<PruebaEntity>) pruebaRepository.findAll();
     }
 
-    @Service
-    public class SubirDataService {
-
-        private final Logger logger = LoggerFactory.getLogger(SubirDataService.class);
-
-        @Autowired
-        private pruebaRepository pruebaRepository;
-
-        @Autowired
-        private estudianteRepository estudianteRepository;
-
-        @Generated
-        public ArrayList<PruebaEntity> ObtenerDatosPruebas() {
-            return (ArrayList<PruebaEntity>) pruebaRepository.findAll();
+    @Generated
+    public String VerificarArchivo(MultipartFile file) {
+        if (file.isEmpty()) {
+            return "Archivo vacío";
         }
 
-        @Generated
-        public String VerificarArchivo(MultipartFile file) {
-            if (file.isEmpty()) {
-                return "Archivo vacío";
-            }
+        try {
+            InputStreamReader reader = new InputStreamReader(file.getInputStream());
+            CSVReader csvReader = new CSVReader(reader);
+            String[] nextLine;
+            while ((nextLine = csvReader.readNext()) != null) {
+                if (nextLine.length != 3) {
+                    return "El archivo debe poseer 3 columnas: Rut, puntaje, fecha";
+                }
+                String rut = nextLine[0];
+                String puntaje = nextLine[1];
+                String fecha = nextLine[2];
 
-            try {
-                InputStreamReader reader = new InputStreamReader(file.getInputStream());
-                CSVReader csvReader = new CSVReader(reader);
-                String[] nextLine;
-                while ((nextLine = csvReader.readNext()) != null) {
-                    if (nextLine.length != 3) {
-                        return "El archivo debe poseer 3 columnas: Rut, puntaje, fecha";
-                    }
-                    String rut = nextLine[0];
-                    String puntaje = nextLine[1];
-                    String fecha = nextLine[2];
-
-                    /*Se verifica que puntaje sea un número*/
-                    if (!puntaje.matches("^[0-9]+$")) {
-                        return "Puntaje debe ser un número";
-                    }
-
-                    /*Se verifica que fecha siga el formato de fecha*/
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-                    try {
-                        dateFormat.parse(fecha);
-                    } catch (ParseException e) {
-                        return "El campo 'fecha' no tiene un formato de fecha válido (dd-MM-yyyy).";
-                    }
+                /*Se verifica que puntaje sea un número*/
+                if (!puntaje.matches("^[0-9]+$")) {
+                    return "Puntaje debe ser un número";
                 }
 
-                /*Si cumple el formato se entrega string vacío*/
-                return "";
-            } catch (IOException | CsvValidationException e) {
-                return "Error al intentar procesar archivo";
+                /*Se verifica que fecha siga el formato de fecha*/
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                try {
+                    dateFormat.parse(fecha);
+                } catch (ParseException e) {
+                    return "El campo 'fecha' no tiene un formato de fecha válido (dd-MM-yyyy).";
+                }
             }
+
+            /*Si cumple el formato se entrega string vacío*/
+            return "";
+        } catch (IOException | CsvValidationException e) {
+            return "Error al intentar procesar archivo";
         }
+    }
 
-        @Generated
-        public String GuardarNombreArchivo(MultipartFile file) {
-            String filename = file.getOriginalFilename();
-            if (filename != null) {
-                if (!file.isEmpty()) {
-                    try {
-                        byte[] bytes = file.getBytes();
-                        Path path = Paths.get(file.getOriginalFilename());
-                        Files.write(path, bytes);
-                        logger.info("Archivo guardado");
-                    } catch (IOException e) {
-                        logger.error("ERROR", e);
-                    }
+    @Generated
+    public String GuardarNombreArchivo(MultipartFile file) {
+        String filename = file.getOriginalFilename();
+        if (filename != null) {
+            if (!file.isEmpty()) {
+                try {
+                    byte[] bytes = file.getBytes();
+                    Path path = Paths.get(file.getOriginalFilename());
+                    Files.write(path, bytes);
+                    logger.info("Archivo guardado");
+                } catch (IOException e) {
+                    logger.error("ERROR", e);
                 }
-                return "Archivo guardado con exito!";
-            } else {
-                return "No se pudo guardar el archivo";
             }
+            return "Archivo guardado con exito!";
+        } else {
+            return "No se pudo guardar el archivo";
         }
+    }
 
-        @Generated
-        public void LeerArchivoCsv(String direccion) {
-            String texto = "";
-            BufferedReader bf = null;
-            try {
-                bf = new BufferedReader(new FileReader(direccion));
-                String temp = "";
-                String bfRead;
-                int count = 1;
-                while ((bfRead = bf.readLine()) != null) {
-                    if (count == 1) {
-                        count = 0;
-                    } else {
-                        GuardarPruebaEnBD(bfRead.split(";")[0], bfRead.split(";")[1], bfRead.split(";")[2]);
-                        temp = temp + "\n" + bfRead;
-                    }
+    @Generated
+    public void LeerArchivoCsv(String direccion) {
+        String texto = "";
+        BufferedReader bf = null;
+        try {
+            bf = new BufferedReader(new FileReader(direccion));
+            String temp = "";
+            String bfRead;
+            int count = 1;
+            while ((bfRead = bf.readLine()) != null) {
+                if (count == 1) {
+                    count = 0;
+                } else {
+                    GuardarPruebaEnBD(bfRead.split(";")[0], bfRead.split(";")[1], bfRead.split(";")[2]);
+                    temp = temp + "\n" + bfRead;
                 }
-                texto = temp;
-                System.out.println("Archivo leido exitosamente");
-            } catch (Exception e) {
-                System.err.println("No se encontro el archivo");
-            } finally {
-                if (bf != null) {
-                    try {
-                        bf.close();
-                    } catch (IOException e) {
-                        logg.error("ERROR", e);
-                    }
+            }
+            texto = temp;
+            System.out.println("Archivo leido exitosamente");
+        } catch (Exception e) {
+            System.err.println("No se encontro el archivo");
+        } finally {
+            if (bf != null) {
+                try {
+                    bf.close();
+                } catch (IOException e) {
+                    logger.error("ERROR", e);
                 }
             }
         }
+    }
 
-        @Generated
-        public void GuardarPruebaEnBD(String rut, String Puntaje, String Fecha_Realizacion) {
+    @Generated
+    public void GuardarPruebaEnBD(String rut, String Puntaje, String Fecha_Realizacion) {
+        PruebaEntity Prueba = new PruebaEntity();
+        estudianteEntity estudiante;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        estudiante = estudianteRepository.findEstudianteByRut(rut);
+        if (estudiante == null) {
+            return;
+        }
+        Prueba.setIdEstudiante(estudiante.getIdEstudiante());
+        if (Objects.equals(Puntaje, "")) {
+            Prueba.setPuntaje_obtenido(150);
+        } else if (Integer.parseInt(Puntaje) < 150 || Integer.parseInt(Puntaje) > 1000) {
+            Prueba.setPuntaje_obtenido(150);
+        } else {
+            Prueba.setPuntaje_obtenido(Integer.parseInt(Puntaje));
+        }
+        Prueba.setFecha_examen(LocalDate.parse(Fecha_Realizacion, formatter));
+        Prueba.setFecha_resultados(LocalDate.now());
+        pruebaRepository.save(Prueba);
+    }
+
+    @Generated
+    public void EliminarPruebas(ArrayList<PruebaEntity> Pruebas) {
+        pruebaRepository.deleteAll(Pruebas);
+    }
+
+
+    public ArrayList<PruebaEntity> ObtenerPruebasPorRutEstudiante(String Rut) {
+        estudianteEntity estudiante = estudianteRepository.findEstudianteByRut(Rut);
+        if (estudiante == null) {
+            ArrayList<PruebaEntity> listafinal = new ArrayList<>();
             PruebaEntity Prueba = new PruebaEntity();
-            estudianteEntity estudiante;
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-            estudiante = estudianteRepository.findEstudianteByRut(rut);
-            if (estudiante == null) {
-                return;
-            }
-            Prueba.setIdEstudiante(estudiante.getIdEstudiante());
-            if (Objects.equals(Puntaje, "")) {
-                Prueba.setPuntaje_obtenido(150);
-            }
-            else if (Integer.parseInt(Puntaje) < 150 || Integer.parseInt(Puntaje) > 1000) {
-                Prueba.setPuntaje_obtenido(150);
-            } else {
-                Prueba.setPuntaje_obtenido(Integer.parseInt(Puntaje));
-            }
-            Prueba.setFecha_examen(LocalDate.parse(Fecha_Realizacion, formatter));
-            Prueba.setFecha_resultados(LocalDate.now());
-            pruebaRepository.save(Prueba);
+            Prueba.setPuntaje_obtenido(-1);
+            listafinal.add(Prueba);
+
+            return listafinal;
+        } else {
+            return pruebaRepository.findEstudentByid(estudiante.getIdEstudiante());
         }
+    }
 
-        @Generated
-        public void EliminarPruebas(ArrayList<PruebaEntity> Pruebas) {
-            pruebaRepository.deleteAll(Pruebas);
-        }
-
-
-        public ArrayList<PruebaEntity> ObtenerPruebasPorRutEstudiante(String Rut) {
-            estudianteEntity estudiante = estudianteRepository.findEstudianteByRut(Rut);
-            if (estudiante == null) {
-                ArrayList<PruebaEntity> listafinal = new ArrayList<>();
-                PruebaEntity Prueba = new PruebaEntity();
-                Prueba.setPuntaje_obtenido(-1);
-                listafinal.add(Prueba);
-
-                return listafinal;
-            } else {
-                return pruebaRepository.findEstudentByid(estudiante.getIdEstudiante());
+    public Integer PromediosPruebasEstudiante(ArrayList<PruebaEntity> Pruebas) {
+        int i = 0;
+        Integer Suma = 0;
+        if (Pruebas.size() > 0) {
+            while (i < Pruebas.size()) {
+                Suma = Suma + Pruebas.get(i).getPuntaje_obtenido();
+                i++;
             }
-        }
-        public Integer PromediosPruebasEstudiante(ArrayList<PruebaEntity> Pruebas) {
-            int i = 0;
-            Integer Suma = 0;
-            if (Pruebas.size() > 0) {
-                while (i < Pruebas.size()) {
-                    Suma = Suma + Pruebas.get(i).getPuntaje_obtenido();
-                    i++;
-                }
-                return (Suma / Pruebas.size());
-            } else {
-                return 0;
-            }
+            return (Suma / Pruebas.size());
+        } else {
+            return 0;
         }
     }
 }
